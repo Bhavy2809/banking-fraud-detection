9d6b6ad07d987b9e4eb53e9cd47428b97e2796ec
bhshukla <shuklabhavy90@gmail.com>
2026-05-31 00:59:24 +0530

    feat: initialize banking fraud system backend

---
+++ src/main/java/com/bhavy/banking_fraud_system/BankingFraudSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bhavy.banking_fraud_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingFraudSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingFraudSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavy/banking_fraud_system/BankingFraudSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavy.banking_fraud_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingFraudSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
